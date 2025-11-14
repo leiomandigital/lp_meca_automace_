40d2beb9d04cf2e46d0fd8f1f91ed6f40266e9b6
Leioman <leiomandigital@gmail.com>
2025-11-14 10:44:46 +0000

    tailwind.config.js com algumas alterações

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 import type {Config} from 'tailwindcss';
 
 export default {
-  darkMode: ['class'],
+  darkMode: ['class', '.dark'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,css}',
+    './index.html',
   ],
   theme: {
     extend: {
